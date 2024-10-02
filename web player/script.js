232dd30d94f437da66cc05096457581cf2b15072
MohammedAbuFoul <106703644+MohammedAbuFoul@users.noreply.github.com>
2024-10-02 20:50:15 +0300

    added script & buttons to flip pages

--- web player/script.js
+++ web player/script.js
@@ -2,16 +2,19 @@ function changepage(a) {
     const RightPage = document.getElementById("RightPage");
     const LeftPage = document.getElementById("LeftPage");
 
-    console.log('aaa')
-    if (a == '+') {
-        RightPage.src = "img/5.png";
-        LeftPage.src = "img/6.png";
+    console.log(`"img/`+(parseInt(RightPage.dataset.page)+1)+`.png"`)
+    let pagenum = parseInt(RightPage.dataset.page)
+    if (a == '+' && pagenum != 603) {
+    console.log(`img/`+(parseInt(RightPage.dataset.page)+1)+`.png`)
+        RightPage.src = `img/`+(pagenum+2)+`.png`;
+        RightPage.dataset.page = pagenum+2;
+        LeftPage.src = `img/`+(pagenum+3)+`.png`;
+
     }
-    if (a == '-') {
-        RightPage.src = "img/1.png";
-        LeftPage.src = "img/2.png";
-
-
+    if (a == '-' && pagenum != 1) {
+        RightPage.src = `img/`+(pagenum-2)+`.png`;
+        RightPage.dataset.page = pagenum-2;
+        LeftPage.src = `img/`+(pagenum-1)+`.png`;
     }
 
 }
